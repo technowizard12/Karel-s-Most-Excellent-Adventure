3c9350eab1a72217186a02841d417447618d1d1e
Simon <technowizard12@gmail.com>
2011-10-18 09:10:05 -0700

    edited some code

--- Adventurer.java
+++ Adventurer.java
@@ -18,6 +18,27 @@ public class Adventurer extends Robot
     }
     // methods for class Adventurer
     
+    /*findMine()
+     * findMine() is the main class for the Excellent Adventure. It completes the problem.
+     * preconditions: robot is in the Lost Beeper Mine universe
+     * postconditions: robot has picked up all beepers in the Lost Beeper Mine.
+     */
+    
+    public void findMine()
+    {
+        walkNorth();
+        walkEast();
+        followTrail();
+        walkToWall();
+        walkEastToWall();
+        followTreasureMapTrail();
+        walkForTen();
+        followWallRight();
+        leftUntilClear();
+        afterMazeDilemma();
+        pickAllMineBeepers();
+    }
+    
     /*walkNorth()
      * walkNorth() moves the robot one space to the North, ending facing North
      * preconditions: Robot is not near a wall directly to the North
@@ -67,6 +88,45 @@ public class Adventurer extends Robot
         while (!facingEast())
         {
             turnLeft();
+        }
+    }
+    
+    /*followTrail()
+     * follows the first trail in the Excellent Adventure.
+     * preconditions: robot is one corner away from beeper trail, facing in the direction of the trail.
+     * postconditions: robot is at the end of the beeper trail, having picked up all the beepers within it.
+     */
+    
+    public void followTrail()
+    {
+        move();
+        while (!nextToTwoBeepers())
+        {
+            pickBeeper();
+            move();
+        }
+    }
+    
+    /*nextToTwoBeppers()
+     * determines whether the robot is standing on top of two beepers.
+     * preconditions: robot is next to at least one beeper
+     * postconditions: the robot has returned true if there are two or more beepers, false if there is 
+     * only one
+     */
+    
+    
+    public boolean nextToTwoBeepers()
+    {
+        pickBeeper();
+        if (nextToABeeper())
+        {
+            putBeeper();
+            return true;
+        }
+        else
+        {
+            putBeeper();
+            return false;
         }
     }
 }
